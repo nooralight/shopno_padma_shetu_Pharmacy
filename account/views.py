0473acdc0d28b4f5dcfce284aaaa01a541f4ff71
aalagbad <aalagbad@iu.edu>
2022-11-21 22:50:33 +0600

    user create

--- account/views.py
+++ account/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User,Seller,Company
 
 # Create your views here.
 def gotoRegistration(request):
@@ -6,3 +7,14 @@ def gotoRegistration(request):
 
 def gotoLogin(request):
     return render(request, 'login.html')
+
+
+def registerUser(request):
+    f_name = request.POST.get("f_name")
+    l_name = request.POST.get("l_name")
+    email = request.POST.get("email")
+    phone = request.POST.get("phone")
+    password = request.POST.get("password")
+    user = User.objects.create(first_name = f_name, last_name = l_name, email = email, phone= phone, password = password)
+    user.save()
+    return redirect('gotoLogin')
