7eed67fc269e783660610966d57090c8f0ea15e0
aalagbad <aalagbad@iu.edu>
2022-12-22 21:13:04 +0600

    edited

--- normal_user/urls.py
+++ normal_user/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns=[
     path('',views.gotoHome, name= 'gotoHome'),
+    path('vendors/',views.gotoVendors, name= 'gotoVendor'),
+    path('about/',views.gotoAbout, name= 'gotoAbout'),
+    path('contact/',views.gotoContact, name= 'gotoContact'),
+    path('products/',views.gotoProducts, name= 'gotoProducts'),
+    path('shop_products/',views.gotoShopProducts,name = 'gotoShop'),
+    path('product_details/',views.gotoProductDetails, name= 'productDetails'),
+
 ]
--- normal_user/views.py
+++ normal_user/views.py
@@ -3,3 +3,22 @@ from django.shortcuts import render, redirect
 # Create your views here.
 def gotoHome(request):
     return render(request, 'index.html')
+
+def gotoVendors(request):
+    return render(request,'vendors.html')
+
+
+def gotoProducts(request):
+    return render(request,'products.html')
+
+def gotoShopProducts(request):
+    return render(request,'shop_product_grid.html')
+
+def gotoContact(request):
+    return render(request, 'contact.html')
+
+def gotoAbout(request):
+    return render(request,'about.html')
+
+def gotoProductDetails(request):
+    return render(request,'product_details.html')
